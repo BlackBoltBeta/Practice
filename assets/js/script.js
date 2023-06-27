fedb6c1a8aa94275df7c5cda5b53dcbf1572ba95
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-06-27 14:19:36 +0000

    AAAAAA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,66 @@
-
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+let dicesix = {
+  sides: 6,
+  roll: function () {
+    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
   }
+}
 
-  let rollDice = () => getRandomIntInclusive(1, 6);
-
-  document.getElementById("roll").addEventListener("click", function () {
-    
-    let player1Score = rollDice();
-
-    let result = "";
-
-    document.getElementById("results").innerHTML = <p>${result}</p>;
+let diceeight = {
+  sides: 8,
+  roll: function () {
+    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
   }
+}
+
+let diceten = {
+  sides: 10,
+  roll: function () {
+    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+let dicetwenty = {
+  sides: 20,
+  roll: function () {
+    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+
+
+//Print diceroll
+
+function printNumber(number) {
+  let placeholder = document.getElementById('placeholder');
+  placeholder.innerHTML = number;
+}
+
+let button = document.getElementById('button');
+
+button.onclick = function() {
+  
+
+
+let diceSide = document.getElementById('diceSide');
+     let selected = diceSide.options[mySelect.selectedIndex].text;
+     if(selected === 'sixSided'){
+      let result = dicesix.roll();
+     } else if (selected === 'eightSided') {
+      let result = diceeight.roll();
+    } else if (selected === 'tenSided') {
+      let result = diceten.roll();
+    } else if (selected === 'twentySided') {
+      let result = dicetwenty.roll();
+    } else {
+
+    }
+    printNumber(result);
+  };
+
 
 
 
